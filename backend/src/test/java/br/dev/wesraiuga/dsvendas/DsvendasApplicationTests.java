a9d1d8c189bd194abadaab94fd7cc87a09a92242
WesRaiuga <weslley_wenny@hotmail.com>
2021-05-03 21:20:10 -0300

    start: projetos criados

---
+++ backend/src/test/java/br/dev/wesraiuga/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.wesraiuga.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
